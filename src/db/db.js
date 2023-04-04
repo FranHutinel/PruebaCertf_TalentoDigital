b8b731cbe7c1950d6c3028cc405710e03cce099f
FranHutinel <m.ohutinel@gmail.com>
2023-04-04 18:19:25 -0400

    primer commit

---
+++ src/db/db.js
@@ -0,0 +1,5 @@
+import {Sequelize}  from "sequelize";
+export const sequelize  = new Sequelize ('ohvhjqfc','ohvhjqfc','fxXGREzKybjqMA_S9sPdHezIZHyv4Nr9',{
+    host : 'mahmud.db.elephantsql.com',
+    dialect:'postgres'
+})
